1e4ce8b6f498d189eee2d4d9a204dd79eeaacf14
smartinsdev <sinval.msjunior@gmail.com>
2022-11-13 12:39:36 -0300

    style: add animation in dropbox cart component

--- components/ui/DropBox.tsx
+++ components/ui/DropBox.tsx
@@ -7,7 +7,7 @@ export function DropBox({ openCart }: Props) {
     <div
       className={`${
         openCart ? "flex" : "hidden"
-      } w-[95%] h-52 border border-gray-300 flex-col absolute right-1/2 translate-x-1/2 top-20 z-10 mt-3 rounded-lg bg-white shadow-2xl p-0 md:mt-6 md:px-4 md:w-80 md:right-1 md:top-14 md:translate-x-0`}
+      } dropbox_animated--mobile md:dropbox_animated--desktop w-[95%] h-52 border border-gray-300 flex-col absolute right-1/2 translate-x-1/2 top-20 z-10 mt-3 rounded-lg bg-white shadow-2xl p-0 md:mt-6 md:px-4 md:w-80 md:right-1 md:top-14 md:translate-x-0`}
     >
       <div className="border-b border-neutral-100">
         <h3 className="text-sm font-bold py-4 px-6">Cart</h3>
