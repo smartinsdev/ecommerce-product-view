3face61a4e4b1b5dfac24095260f73e1494b793d
smartinsdev <sinval.msjunior@gmail.com>
2022-11-14 17:00:50 -0300

    test: header fixed top

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,7 +2,7 @@ import { NavBar } from "./NavBar";
 
 export function Header() {
   return (
-    <header className="w-full h-20 border-b border-neutral-100">
+    <header className="max-w-7xl w-full h-20 border-b border-neutral-100">
       <NavBar />
     </header>
   );
