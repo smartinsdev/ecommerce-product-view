305e2994cd501e70d7eee846e164256436583f09
smartinsdev <sinval.msjunior@gmail.com>
2022-11-14 17:08:37 -0300

    feat: add un array of images for Slider component

---
+++ constants/images.ts
@@ -0,0 +1,35 @@
+import image1 from "../public/images/image-product-1.jpg";
+import image1Thumbnail from "../public/images/image-product-1-thumbnail.jpg";
+import image2 from "../public/images/image-product-2.jpg";
+import image2Thumbnail from "../public/images/image-product-2-thumbnail.jpg";
+import image3 from "../public/images/image-product-3.jpg";
+import image3Thumbnail from "../public/images/image-product-3-thumbnail.jpg";
+import image4 from "../public/images/image-product-4.jpg";
+import image4Thumbnail from "../public/images/image-product-4-thumbnail.jpg";
+
+export const images = [
+  {
+    id: 1,
+    src: image1,
+    thumbnail: image1Thumbnail,
+    alt: "Image of product",
+  },
+  {
+    id: 2,
+    src: image2,
+    thumbnail: image2Thumbnail,
+    alt: "Image of product",
+  },
+  {
+    id: 3,
+    src: image3,
+    thumbnail: image3Thumbnail,
+    alt: "Image of product",
+  },
+  {
+    id: 4,
+    src: image4,
+    thumbnail: image4Thumbnail,
+    alt: "Image of product",
+  },
+];
